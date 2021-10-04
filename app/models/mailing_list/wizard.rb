a0f6647aac2630f6a4f922f4dd75f8c43b6b7bde
Ross Oliver <ross.oliver36@gmail.com>
2021-10-04 10:57:42 +0100

    Add new age step to mailing list wizard
    We want to display a new age step as part of the mailing list sign up so
    that we can collect information about a candidates age.
    
    Before we collect this data we want to evaluate three different methods for
    collecting age-related information, in order to determine how the user
    responds (does one method cause more drop-off during sign up than another).
    The methods are:
    
    - Date of birth
    - Year of birth
    - Age range
    
    We are not currently persisting the data entered by the user.

--- app/models/mailing_list/wizard.rb
+++ app/models/mailing_list/wizard.rb
@@ -8,6 +8,7 @@ module MailingList
       Steps::Name,
       ::Wizard::Steps::Authenticate,
       Steps::AlreadySubscribed,
+      Steps::Age,
       Steps::DegreeStatus,
       Steps::TeacherTraining,
       Steps::Subject,
